1bd9198a7c00d1620fd11a796f422cb1912072e2
vaishnavisolanke08 <vaishnavisolanke009@gmail.com>
2026-01-09 19:10:23 +0530

    success

---
+++ DSA Code/practice.java
@@ -0,0 +1,7 @@
+package DSA Code;
+
+public class practice {
+    
+}
+git config --global user.name "vaishnavisolanke08"
+git config --global user.email "vaishnavisolanke009@gmail.com
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # 30-Days-DSA-Challenge-
 Here, I'll be uploading 5 DSA problems with solutions. 
+i am vaishnavi 
