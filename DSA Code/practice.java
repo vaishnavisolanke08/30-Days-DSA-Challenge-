70fe97f424b76f2fb5b61f1b41d2bf33b2d5e9d4
vaishnavisolanke08 <vaishnavisolanke009@gmail.com>
2026-01-10 19:47:11 +0530

    success

--- DSA Code/practice.java
+++
@@ -1,7 +0,0 @@
-package DSA Code;
-
-public class practice {
-    
-}
-git config --global user.name "vaishnavisolanke08"
-git config --global user.email "vaishnavisolanke009@gmail.com
---
+++ Learn the basics/Thing to know in java/UserInput.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class UserInput{
+    public static void main (String[]args){
+        Scanner sc = new Scanner(System.in);
+            int age = sc.nextInt();
+            System.out.println(age);
+        }
+    }
