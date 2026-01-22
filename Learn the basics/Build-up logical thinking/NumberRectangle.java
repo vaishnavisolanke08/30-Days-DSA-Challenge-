c37dacf2547241ce9ae9e67df64369199bdbee29
vaishnavisolanke08 <vaishnavisolanke009@gmail.com>
2026-01-22 12:27:10 +0530

    success

---
+++ Learn the basics/Build-up logical thinking/NumberRectangle.java
@@ -0,0 +1,15 @@
+
+public class NumberRectangle {
+    public static void main (String[]args){
+        int row =3;
+        int col = 4;
+
+        for( int i = 1; i <=row ; i++){
+            for(int j=1; j <=col;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
---
+++ Learn the basics/Build-up logical thinking/Rectangle.java
@@ -0,0 +1,13 @@
+public class Rectangle{
+    public static void main(String[]args){
+        int row = 4;
+        int col = 5;
+
+        for(int i = 1; i<=4; i++){
+            for(int j =1; j<=5;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
