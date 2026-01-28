2fc718ef83ae7cab62ee709be2f80af3878e8972
vaishnavisolanke08 <vaishnavisolanke009@gmail.com>
2026-01-28 18:04:48 +0530

    success

---
+++ Learn the basics/Basic maths/PrimeNo.java
@@ -0,0 +1,18 @@
+
+public class PrimeNo {
+    public static void main (String[]args){
+        int num = 5;
+        int count = 0;
+
+        for(int i = 1; i<= num; i++){
+            if(num%i ==0);
+            count++;
+        }
+        if(count == 2){
+            System.out.println("its prime");
+        }else{
+            System.out.println("its not prime");
+        }
+    }
+    
+}
